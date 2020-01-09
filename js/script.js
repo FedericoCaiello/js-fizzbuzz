fab9038fc5502fe08ec23dcfad33146d13f406c5
Federico <federico.caiello@gmail.com>
2020-01-09 20:08:33 +0100

    prova con while

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
 
-for (var i = 1; i < 101; i++) {
+// for (var i = 1; i < 101; i++) {
+//
+ // var quoziente = i%3;
+ // var messaggio = i;
+ // var quoziente2 = i%5;
+ // var quoziente3 = i%15;
+//
+// if (quoziente3==0) {
+//   messaggio = 'FizzBuzz';
+// } else if (quoziente2==0) {
+//  messaggio = 'Buzz';
+// } else if (quoziente==0)
+//  messaggio = 'Fizz';
+// console.log(messaggio);
+// }
 
-var quoziente = i%3;
-var messaggio = i
-var quoziente2 = i%5;
-var quoziente3 = i%15;
 
-if (quoziente3==0) {
-  messaggio = 'FizzBuzz';
-} else if (quoziente2==0) {
- messaggio = 'Buzz';
-} else if (quoziente==0)
- messaggio = 'Fizz';
-console.log(messaggio);
+var i = 1;
+while (i < 101){
+  var quoziente = i%3;
+  var messaggio = i;
+  var quoziente2 = i%5;
+  var quoziente3 = i%15;
+  console.log(i);
+  if (quoziente3==0){
+    messaggio = 'FizzBuzz';
+  }else if (quoziente2==0) {
+    messaggio = 'Buzz';
+  }else if (quoziente==0)
+  messaggio='Fizz';
+  console.log(messaggio);
+  i = i + messaggio;
+  i++;
+
+
 
 }
+console.log(i);

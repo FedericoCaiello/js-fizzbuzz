2140417ba76d7416c7cb35e1440ef7236e1ba144
Federico <federico.caiello@gmail.com>
2020-01-16 19:04:51 +0100

    salvato

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
 
 
-// for (var i = 1; i < 101; i++) {
-//
-//  var quoziente = i%3;
-//  var messaggio = i;
-//  var quoziente2 = i%5;
-//  var quoziente3 = i%15;
-//
-// if (quoziente3==0) {
-//   messaggio = 'FizzBuzz';
-// } else if (quoziente2==0) {
-//  messaggio = 'Buzz';
-// } else if (quoziente==0)
-//  messaggio = 'Fizz';
-// console.log(messaggio);
-// }
+for (var i = 1; i < 101; i++) {
 
+ var quoziente = i%3;
+ var messaggio = i;
+ var quoziente2 = i%5;
+ var quoziente3 = i%15;
 
-var i = 1;
-while (i <= 100){
-  quoziente = i%3;
-  quoziente2 = i%5;
-  quoziente3 = i%15;
-  if (quoziente3 == 0){
-    messaggio = 'FizzBuzz';
-    console.log('FizzBuzz');
-  }else if (quoziente2 == 0) {
-    messaggio = 'Buzz';
-    console.log('Buzz');
-  }else if (quoziente == 0){
-    messaggio='Fizz';
-    console.log('Fizz');
-  }
-  else {
-    messaggio = i;
-    console.log(messaggio);
-  }
-  i++;
-
-
+if (quoziente3==0) {
+  messaggio = 'FizzBuzz';
+} else if (quoziente2==0) {
+ messaggio = 'Buzz';
+} else if (quoziente==0)
+ messaggio = 'Fizz';
+console.log(messaggio);
 }
-console.log(i);
+
+
+// var i = 1;
+// while (i <= 100){
+//   quoziente = i%3;
+//   quoziente2 = i%5;
+//   quoziente3 = i%15;
+//   if (quoziente3 == 0){
+//     messaggio = 'FizzBuzz';
+//     console.log('FizzBuzz');
+//   }else if (quoziente2 == 0) {
+//     messaggio = 'Buzz';
+//     console.log('Buzz');
+//   }else if (quoziente == 0){
+//     messaggio='Fizz';
+//     console.log('Fizz');
+//   }
+//   else {
+//     messaggio = i;
+//     console.log(messaggio);
+//   }
+//   i++;
+//
+//
+// }
+// console.log(i);
